094ad2ff6720dde3473f8f194f1c72ee7785c2ff
comedial <comedial@student.21-school.ru>
2024-02-15 18:50:44 +0300

    all

--- Chat/SocketServer/target/classes/messages.sql
+++
@@ -1,2 +0,0 @@
-drop table if exists messages;
-create table if not exists messages (id serial primary key, sender varchar, senderId int, text varchar, time timestamp default current_timestamp);
--- Chat/SocketServer/target/classes/users.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists users;
-create table if not exists users ( id serial primary key,
-                                   email  varchar,
-                                   password varchar);
